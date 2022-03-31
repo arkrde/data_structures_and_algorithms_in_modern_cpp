6cd3379c017af85c185fa7322fc79d389a8b1cf0
Arnab De <arkrde@gmail.com>
2022-03-31 19:43:47 +0530

    refactor

--- dsa_cpp/include/utilities/place_extremum_first.hpp
+++ dsa_cpp/include/utilities/place_extremum_first.hpp
@@ -2,28 +2,25 @@
 #define PLACE_EXTREMUM_FIRST_HPP
 
 #include <iterator>
-#include "utilities/iter_compare_and_swap.hpp"
+#include "utilities/extremum.hpp"
 
 namespace utilities {
 
-using utilities::iter_compare_and_swap;
+using std::iter_swap;
+using utilities::extremum;
 
 template <typename RandomAccessIter, typename Comp>
-void
-place_extremum_first(RandomAccessIter first,
-                    RandomAccessIter last,
-                    Comp comparator) {
-  if (first == last) {
-    return;
-  }
-  if (last == first + 1) {
-    return;
-  }
-  for (auto iter = last - 1; iter != first; --iter) {
-    iter_compare_and_swap(iter, iter - 1, comparator);
-  }
+void place_extremum_first(RandomAccessIter first, RandomAccessIter last,
+                          Comp comparator) {
+    if (first == last || last == first + 1) {
+        return;
+    }
+    RandomAccessIter ext = extremum(first, last, comparator);
+    if (ext != first) {
+        iter_swap(first, ext);
+    }
 }
 
 } // namespace utilities
 
-#endif // PLACE_MINIMUM_FIRST_HPP
+#endif
