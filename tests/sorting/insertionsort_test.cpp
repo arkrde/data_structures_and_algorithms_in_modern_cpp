c5b4f023a92ee71ffde0e96acb75b9f303382da5
Arnab De <arkrde@gmail.com>
2022-03-31 19:41:38 +0530

    testcase for insertion sort

--- tests/sorting/insertionsort_test.cpp
+++ tests/sorting/insertionsort_test.cpp
@@ -9,41 +9,39 @@ static constexpr int ShortSequenceLength = 10;
 static constexpr int LongSequenceLength = 10000;
 
 TEST(SortEval, ShortSequenceAscending) {
-  std::vector<int> array(ShortSequenceLength);
-  std::generate(std::begin(array), std::end(array), []() {
-    return static_cast<int>(1000.0 * rand() / RAND_MAX);
-  });
-  insertion_sort(array.begin(), array.end());
-  EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend()), true);
+    std::vector<int> array(ShortSequenceLength);
+    std::generate(std::begin(array), std::end(array), []() {
+        return static_cast<int>(1000.0 * rand() / RAND_MAX);
+    });
+    insertion_sort(array.begin(), array.end());
+    EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend()), true);
 }
 
 TEST(SortEval, ShortSequenceDescending) {
-  std::vector<int> array(ShortSequenceLength);
-  std::generate(std::begin(array), std::end(array), []() {
-    return static_cast<int>(1000.0 * rand() / RAND_MAX);
-  });
-  insertion_sort(array.begin(), array.end(), std::greater<int>());
-  EXPECT_EQ(
-      std::is_sorted(array.cbegin(), array.cend(), std::greater<int>()),
-      true);
+    std::vector<int> array(ShortSequenceLength);
+    std::generate(std::begin(array), std::end(array), []() {
+        return static_cast<int>(1000.0 * rand() / RAND_MAX);
+    });
+    insertion_sort(array.begin(), array.end(), std::greater<int>());
+    EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend(), std::greater<int>()),
+              true);
 }
 
 TEST(SortEval, LongSequenceAscending) {
-  std::vector<int> array(LongSequenceLength);
-  std::generate(std::begin(array), std::end(array), []() {
-    return static_cast<int>(1000.0 * rand() / RAND_MAX);
-  });
-  insertion_sort(array.begin(), array.end());
-  EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend()), true);
+    std::vector<int> array(LongSequenceLength);
+    std::generate(std::begin(array), std::end(array), []() {
+        return static_cast<int>(1000.0 * rand() / RAND_MAX);
+    });
+    insertion_sort(array.begin(), array.end());
+    EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend()), true);
 }
 
 TEST(SortEval, LongSequenceDescending) {
-  std::vector<int> array(LongSequenceLength);
-  std::generate(std::begin(array), std::end(array), []() {
-    return static_cast<int>(1000.0 * rand() / RAND_MAX);
-  });
-  insertion_sort(array.begin(), array.end(), std::greater<int>());
-  EXPECT_EQ(
-      std::is_sorted(array.cbegin(), array.cend(), std::greater<int>()),
-      true);
+    std::vector<int> array(LongSequenceLength);
+    std::generate(std::begin(array), std::end(array), []() {
+        return static_cast<int>(1000.0 * rand() / RAND_MAX);
+    });
+    insertion_sort(array.begin(), array.end(), std::greater<int>());
+    EXPECT_EQ(std::is_sorted(array.cbegin(), array.cend(), std::greater<int>()),
+              true);
 }
