6b43f957b5206752e85e762456987e6668b2ea32
Arnab De <arkrde@gmail.com>
2022-03-31 19:42:40 +0530

    formatting

--- dsa_cpp/include/sorting/insertionsort.hpp
+++ dsa_cpp/include/sorting/insertionsort.hpp
@@ -15,16 +15,16 @@ using utilities::place_extremum_first;
  * C++ - Section 6.3 Insetion Sort
  */
 template <typename RandomAccessIter, typename Comparator>
-void
-insertion_sort(RandomAccessIter first, RandomAccessIter last, Comparator comp) {
+void insertion_sort(RandomAccessIter first, RandomAccessIter last,
+                    Comparator comp) {
     if (first == last) {
         return;
     }
     if (last == first + 1) {
         return;
     }
-    // A first pass through the array to place the smallest/largest key in the first
-    // position depending upon the `comp` function. 
+    // A first pass through the array to place the smallest/largest key in the
+    // first position depending upon the `comp` function.
     place_extremum_first(first, last, comp);
     // Adaptive sort
     for (auto iter = first + 2; iter != last; ++iter) {
@@ -43,12 +43,11 @@ insertion_sort(RandomAccessIter first, RandomAccessIter last, Comparator comp) {
  * C++ - Section 6.3 Insetion Sort
  */
 template <typename RandomAccessIter>
-void
-insertion_sort(RandomAccessIter first, RandomAccessIter last) {
+void insertion_sort(RandomAccessIter first, RandomAccessIter last) {
     using value_type =
         typename std::iterator_traits<RandomAccessIter>::value_type;
     insertion_sort(first, last, std::less<value_type>());
 }
-} // namespace sort
+} // namespace sorting
 
-#endif // INSERTION_SORT_HPP
+#endif 
