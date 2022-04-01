ec46d2f2362c92f4cee68834fba6667511dd473a
Arnab De <arkrde@gmail.com>
2022-04-01 23:00:10 +0530

    refactored for multiway partitioning

--- dsa_cpp/include/sorting/quicksort.hpp
+++ dsa_cpp/include/sorting/quicksort.hpp
@@ -8,40 +8,42 @@
 namespace sorting {
 
 template <typename Iter, typename Comparator, typename PartitionPolicy>
-void quicksort(Iter begin, Iter end, PartitionPolicy partition,
-               Comparator comp, size_t subfile_size = 1) {
-    if (end - begin <= 1) {
-        return;
-    }
-    using sorting::insertion_sort;
-
+void quicksort(Iter begin, Iter end, PartitionPolicy partition, Comparator comp,
+               size_t subfile_size) {
     if (end - begin < subfile_size) {
-        insertion_sort(begin, end, comp);
         return;
     }
-    auto iter = partition(begin, end, comp, subfile_size);
-    if (end - iter > iter - begin) {
-        quicksort(begin, iter, partition, comp);
-        quicksort(iter + 1, end, partition, comp);
-    } else {
-        quicksort(iter + 1, end, partition, comp);
-        quicksort(begin, iter, partition, comp);
+    std::vector<Iter> pivots = partition(begin, end, comp, subfile_size);
+    
+    auto e = pivots.begin();
+    auto b = begin;
+    while (e != pivots.end()) {
+        quicksort(b, *e, partition, comp, subfile_size);
+        b = *e + 1;
+        ++e;
     }
+    quicksort(b, end, partition, comp, subfile_size);
+    if (subfile_size > 1) {
+        insertion_sort(begin, end, comp);
+    }
+    
 }
 
 template <typename Iter, typename Comparator>
-void two_way_quicksort(Iter begin, Iter end, Comparator comp, size_t subfile_size = 1) {
-    using quicksort::two_way_partition;
-    return quicksort(begin, end, two_way_partition<Iter, Comparator>(), comp, subfile_size);
+void two_way_quicksort(Iter begin, Iter end, Comparator comp,
+                       size_t subfile_size = 1) {
+    using partition_policy::two_way_partition;
+    quicksort(begin, end, two_way_partition<Iter, Comparator>(), comp,
+              subfile_size);
 }
 
 template <typename Iter>
 void two_way_quicksort(Iter begin, Iter end) {
-    using quicksort::two_way_partition;
+    using partition_policy::two_way_partition;
     using value_type = typename std::iterator_traits<Iter>::value_type;
     std::less<value_type> comp;
-    return quicksort(begin, end,
-                     two_way_partition<Iter, std::less<value_type>>(), comp, 1);
+    quicksort(begin, end, two_way_partition<Iter, std::less<value_type>>(),
+              comp, 1);
 }
 
 } // namespace sorting
