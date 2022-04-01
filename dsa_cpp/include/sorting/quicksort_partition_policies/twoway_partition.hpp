f41c16ef977d35ec3778e37387a484e5ae807fb8
Arnab De <arkrde@gmail.com>
2022-04-01 23:01:06 +0530

    refactored for multiway partitioning

--- dsa_cpp/include/sorting/quicksort_partition_policies/twoway_partition.hpp
+++ dsa_cpp/include/sorting/quicksort_partition_policies/twoway_partition.hpp
@@ -1,23 +1,30 @@
 #ifndef TWOWAY_PARTITION_HPP
 #define TWOWAY_PARTITION_HPP
 
-namespace quicksort {
+#include <vector>
+
+namespace partition_policy {
 
 template <typename Iter, typename Comparator>
 class two_way_partition {
 private:
-    Iter partition_impl(Iter begin, Iter end, Comparator comp, size_t subfile_size) {
-        if (end - begin <= 1) return end;
+    std::vector<Iter> partition_impl(Iter begin, Iter end, Comparator comp,
+                                     size_t subfile_size) {
+        std::vector<Iter> ret;
+        if (end - begin < subfile_size) {
+            ret.push_back(end);
+            return ret;
+        }
         auto pivot = --end;
         while (begin < end) {
             // Search from the beginning for the first element larger than the
             // pivot
-            while (comp(*begin, *pivot) && begin < end) {
+            while (begin < end && comp(*begin, *pivot)) {
                 ++begin;
             }
             --end;
             // Search from the end for the first element smaller than the pivot
-            while (comp(*pivot, *end) && begin < end) {
+            while (begin < end && comp(*pivot, *end)) {
                 --end;
             };
 
@@ -29,14 +36,17 @@ private:
         }
         if (comp(*pivot, *end)) {
             std::iter_swap(end, pivot);
-            return end;
+            ret.push_back(end);
+        } else {
+            std::iter_swap(begin, pivot);
+            ret.push_back(begin);
         }
-        std::iter_swap(begin, pivot);
-        return begin;
+        return ret;
     }
 
 public:
-    Iter operator()(Iter begin, Iter end, Comparator comp, size_t subfile_size = 1) {
+    std::vector<Iter> operator()(Iter begin, Iter end, Comparator comp,
+                    size_t subfile_size = 1) {
         return partition_impl(begin, end, comp, subfile_size);
     }
 };
