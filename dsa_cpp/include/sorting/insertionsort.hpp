ffe645cb8850453b631cf76a541f966d0ca819a4
Arnab De <arkrde@gmail.com>
2022-04-07 00:35:25 +0530

    insertion-sort type constraint

--- dsa_cpp/include/sorting/insertionsort.hpp
+++ dsa_cpp/include/sorting/insertionsort.hpp
@@ -1,9 +1,9 @@
 #ifndef INSERTION_SORT_HPP
 #define INSERTION_SORT_HPP
 
-#include <iterator>
-#include <algorithm>
 #include "utilities/place_extremum_first.hpp"
+#include <algorithm>
+#include <iterator>
 
 namespace sorting {
 
@@ -14,40 +14,45 @@ using utilities::place_extremum_first;
  * @brief insertion sort: uses the optimizations from Sedgewick: Algorithms in
  * C++ - Section 6.3 Insetion Sort
  */
-template <typename RandomAccessIter, typename Comparator>
-void insertion_sort(RandomAccessIter first, RandomAccessIter last,
-                    Comparator comp) {
-    if (first == last) {
-        return;
+template <typename RandomIter, typename Comp,
+          typename std::enable_if<std::is_same<
+              typename std::iterator_traits<RandomIter>::iterator_category,
+              std::random_access_iterator_tag>::value> * = nullptr>
+void insertion_sort(RandomIter first, RandomIter last,
+                    Comp comp) {
+  if (first == last) {
+    return;
+  }
+  if (last == first + 1) {
+    return;
+  }
+  // A first pass through the array to place the smallest/largest key in the
+  // first position depending upon the `comp` function.
+  place_extremum_first(first, last, comp);
+  // Adaptive sort
+  for (auto iter = first + 2; iter != last; ++iter) {
+    auto it = iter;
+    auto v = *it;
+    while (comp(v, *(it - 1))) {
+      *it = *(it - 1);
+      --it;
     }
-    if (last == first + 1) {
-        return;
-    }
-    // A first pass through the array to place the smallest/largest key in the
-    // first position depending upon the `comp` function.
-    place_extremum_first(first, last, comp);
-    // Adaptive sort
-    for (auto iter = first + 2; iter != last; ++iter) {
-        auto it = iter;
-        auto v = *it;
-        while (comp(v, *(it - 1))) {
-            *it = *(it - 1);
-            --it;
-        }
-        *it = v;
-    }
+    *it = v;
+  }
 }
 
 /**
  * @brief insertion sort: uses the optimizations from Sedgewick: Algorithms in
  * C++ - Section 6.3 Insetion Sort
  */
-template <typename RandomAccessIter>
-void insertion_sort(RandomAccessIter first, RandomAccessIter last) {
-    using value_type =
-        typename std::iterator_traits<RandomAccessIter>::value_type;
-    insertion_sort(first, last, std::less<value_type>());
+template <typename RandomIter,
+          typename std::enable_if<std::is_same<
+              typename std::iterator_traits<RandomIter>::iterator_category,
+              std::random_access_iterator_tag>::value> * = nullptr>
+void insertion_sort(RandomIter first, RandomIter last) {
+  using value_type = typename std::iterator_traits<RandomIter>::value_type;
+  insertion_sort(first, last, std::less<value_type>());
 }
 } // namespace sorting
 
-#endif 
+#endif
